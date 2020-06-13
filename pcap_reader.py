50dc0e19f9420ed15d64d68753ab1c93902aeb56
swedge <scott.wedge@gmail.com>
2020-06-12 22:38:09 -0400

    Add functions with docstrings but no code yet

--- pcap_reader.py
+++ pcap_reader.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python3
+"""Script that reads packets of selected type from selected pcap file
+   then displays the packets
+"""
 
+# import statements
 from scapy.all import *
+
+
+def list_pcap_files():
+    """Return a list of all files of type .pcap
+    """
+
+def select_pcap_file():
+    """Select one of the listed files
+       Default to DEFAULT_FILE
+    """
+
+
+def list_packet_types():
+    """Return list of packet types in the pcap file
+    """
+
+def select_packet_type():
+    """Return one of the listed packet types
+       If invalid entry, default to DEFAULT_TYPE
+    """
+
 
 # set default PCAP file to "pcap_files/pings.pcap"
 PCAP_FILE = "pcap_files/pings.pcap"
@@ -12,4 +37,3 @@ packets = rdpcap(PCAP_FILE, COUNT)
 
 for p in packets:
     print(p.summary())
-
